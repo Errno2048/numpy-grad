d1ded5330db8e5c9354ec93e5a127f9847cd8ea7
Errno2048 <2282804274@qq.com>
2023-03-11 10:01:02 +0800

    add SGD optimizer, modify Adam optimizer

--- grad/optim.py
+++ grad/optim.py
@@ -32,13 +32,51 @@ class Optimizer:
     def optimize(self, name, tensor):
         return self.lr * tensor._grad
 
+class SGD(Optimizer):
+    def __init__(self, parameters, lr=1e-3, momentum=0.0, dampening=0.0, nesterov=False, weight_decay=0.0):
+        super().__init__(parameters, lr)
+        self.momentum = momentum
+        self.dampening = dampening
+        self.nesterov = nesterov
+        self.weight_decay = weight_decay
+        self.momentum_dicts = {}
+        self.reset()
+
+    def reset(self):
+        super().reset()
+        for name, tensor in self._traverse_parameter():
+            if tensor.requires_grad:
+                self.momentum_dicts[name] = None
+
+    def optimize(self, name, tensor):
+        grad = tensor._grad
+        grad = grad + self.weight_decay * tensor._value
+        if self.momentum != 0.0:
+            prev_b = self.momentum_dicts.get(name, None)
+            if prev_b is None:
+                b = grad
+            else:
+                b = self.momentum * prev_b + (1 - self.dampening) * grad
+            self.momentum_dicts[name] = b
+            if self.nesterov:
+                grad = grad + self.momentum * b
+            else:
+                grad = b
+        return self.lr * grad
+
 class Adam(Optimizer):
-    def __init__(self, parameters, lr=1e-3, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    def __init__(self, parameters, lr=1e-3, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0.0, weight_decay_index=2):
         super().__init__(parameters, lr)
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
+        self.weight_decay = weight_decay
+        self.weight_decay_index = weight_decay_index
         self.momentum_dicts = {}
+        self.reset()
+
+    def reset(self):
+        super().reset()
         for name, tensor in self._traverse_parameter():
             if tensor.requires_grad:
                 self.momentum_dicts[name] = [
@@ -55,4 +93,7 @@ class Adam(Optimizer):
         self.momentum_dicts[name] = [new_m, new_v]
         fix_m = m / (1 - self.beta1 ** t)
         fix_v = v / (1 - self.beta2 ** t)
-        return self.lr * fix_m / (np.sqrt(fix_v) + self.epsilon)
+        res = fix_m / (np.sqrt(fix_v) + self.epsilon)
+        if self.weight_decay != 0.0:
+            res = res + self.weight_decay * abs(tensor._value ** self.weight_decay_index)
+        return self.lr * res
